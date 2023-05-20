cc7563c02aa65d9bf9801714ccdf6c22c0995949
otienosteve <steveotieno701@gmail.com>
2023-05-20 11:04:17 +0300

    Ad Test for Post

--- lib/main.py
+++ lib/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app= FastAPI()
+
+class EmployeeSchema(BaseModel):
+    id : int
+    first_name : str
+    last_name : str
+    email : str
+    age : int
+    gender : str
+    phone_number : int
+    salary : int 
+    designation : str
+
 
 @app.get('/')
 def root() -> None: 
     return {'success' : "root"}
+
+
+@app.post('/add_employee')
+def add_employee(payload: EmployeeSchema) -> EmployeeSchema:
+
+    return {"detail" :"success"}
--- lib/test/test_main.py
+++ lib/test/test_main.py
@@ -1,6 +1,7 @@
 import pytest 
 from fastapi.testclient import TestClient
 from main import app 
+from lib.models.employee import session, Employee
 
 
 
@@ -8,6 +9,28 @@ from main import app
 def Client():
     return TestClient(app)
 
+@pytest.fixture(scope='session')
+def employee():
+
+    yield {
+'id': 707,
+'first_name': 'Purity',
+'last_name': 'Edwards',
+'email': 'Pedwars@jigsy.com',
+'age': 38,
+'gender': 'Female',
+'phone_number': 6469701273,
+'salary': 121396,
+'designation': 'Surveyor'
+}
+    session.query(Employee).filter_by(id=707).delete()
+
 def test_root(Client):
     res=Client.get('/')
     assert res.json() == {'success' : "root"}
+
+def test_post(Client, employee):
+    res = Client.post('/add_employee', headers={'content-type':'application/json','accept':'application/json'} ,json=employee)
+    added = session.query(Employee).filter_by(id=employee.id).first()
+    assert added, f"Post Operation unsuccessful" 
+
